2d69c75b070fa04e82cadbb25a57359c96736af0
Benny Zlotnik <bzlotnik@protonmail.com>
2020-06-24 22:26:20 +0300

    fix vm schema

--- migrations/2020-06-22-163236_create_vms/up.sql
+++ migrations/2020-06-22-163236_create_vms/up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE vms (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(50) UNIQUE,
-    status INT4,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    status INT4 NOT NULL,
     host_id UUID REFERENCES hosts(id),
-    vcpu INTEGER,
-    memory INTEGER,
-    kernel VARCHAR(255),
-    root_file_system VARCHAR(255)
+    vcpu INTEGER NOT NULL,
+    memory INTEGER NOT NULL,
+    kernel VARCHAR(255) NOT NULL,
+    root_file_system VARCHAR(255) NOT NULL
 )
