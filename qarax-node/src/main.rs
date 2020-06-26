a364a26f9c821b6fbff13aba4cc61ccde27b524a
Benny Zlotnik <bzlotnik@protonmail.com>
2020-06-26 11:27:49 +0300

    listen on ipv4

--- qarax-node/src/main.rs
+++ qarax-node/src/main.rs
@@ -7,8 +7,8 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let vm_service = VmService::new();
+    let addr = "0.0.0.0:50051".parse()?;
+    let node = QaraxNode::default();
 
     Server::builder()
         .add_service(NodeServer::new(vm_service))
