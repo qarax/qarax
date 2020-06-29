19a82f1f57e0ee92f325fbb671e98707508bf992
Benny Zlotnik <bzlotnik@protonmail.com>
2020-06-29 20:39:37 +0300

    collect the status of the firecracker process

--- qarax-node/firecracker_rust_sdk/src/models/machine.rs
+++ qarax-node/firecracker_rust_sdk/src/models/machine.rs
@@ -1,8 +1,8 @@
 use super::BootSource;
-use super::MachineConfiguration;
 use super::Drive;
+use super::MachineConfiguration;
 
-use crate::http::client::{VmmClient, Method};
+use crate::http::client::{Method, VmmClient};
 
 use serde_json;
 use tokio;
@@ -18,7 +18,13 @@ pub struct Machine {
     pid: u32,
 }
 impl Machine {
-    pub fn new(socket_path: String, machine_configuration: MachineConfiguration, boot_source: BootSource, drive: Drive, pid: u32) -> Self {
+    pub fn new(
+        socket_path: String,
+        machine_configuration: MachineConfiguration,
+        boot_source: BootSource,
+        drive: Drive,
+        pid: u32,
+    ) -> Self {
         Machine {
             client: VmmClient::new(socket_path),
             machine_configuration,
@@ -32,7 +38,10 @@ impl Machine {
     pub async fn configure_boot_source(&self) -> Result<String> {
         let boot_source = serde_json::to_string(&self.boot_source).unwrap();
         println!("Sending boot_source with {}\n", boot_source);
-        Ok(self.client.request("/boot-source", Method::PUT, &boot_source.as_bytes()).await?)
+        Ok(self
+            .client
+            .request("/boot-source", Method::PUT, &boot_source.as_bytes())
+            .await?)
     }
 
     pub async fn configure_drive(&self) -> Result<String> {
@@ -40,23 +49,35 @@ impl Machine {
         println!("Sending drive with {}\n", drive);
         let drive_id = &self.drive.drive_id;
         let endpoint = format!("/drives/{}", drive_id);
-        Ok(self.client.request(&endpoint, Method::PUT, &drive.as_bytes()).await?)
+        Ok(self
+            .client
+            .request(&endpoint, Method::PUT, &drive.as_bytes())
+            .await?)
     }
 
-    pub async fn start(&self) -> Result<String> {   
+    pub async fn start(&self) -> Result<String> {
         tokio::join!(self.configure_boot_source(), self.configure_drive());
-     
+
         println!("Starting VM :O");
-        Ok(self.client.request("/actions", Method::PUT,b"{\"action_type\": \"InstanceStart\"}").await?)
+        Ok(self
+            .client
+            .request(
+                "/actions",
+                Method::PUT,
+                b"{\"action_type\": \"InstanceStart\"}",
+            )
+            .await?)
     }
 
-    pub async fn stop(&self) -> Result<()> {        
+    pub async fn stop(&self) -> Result<()> {
         // TODO error handling
         use nix::sys::signal;
+        use nix::sys::wait::waitpid;
         use nix::unistd::Pid;
         use std::fs;
 
         signal::kill(Pid::from_raw(self.pid as i32), signal::Signal::SIGTERM)?;
+        waitpid(Pid::from_raw(self.pid as i32), None)?;
         fs::remove_file(&self.client.socket_path).expect("failed to remove file");
 
         Ok(())
