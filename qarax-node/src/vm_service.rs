d484fb1ecc48f57448356c5695656703fa85d1fa
Benny Zlotnik <bzlotnik@protonmail.com>
2020-06-29 22:21:50 +0300

    add logger

--- qarax-node/src/vm_service.rs
+++ qarax-node/src/vm_service.rs
@@ -8,18 +8,20 @@ use std::collections::HashMap;
 
 use std::sync::Arc;
 use tokio::sync::RwLock;
-use tonic::{Request, Response, Status};
+use tonic::{Code, Request, Response, Status};
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct VmService {
     // TODO: Not sure about this at all
     handlers: Arc<RwLock<HashMap<String, VmmHandler>>>,
+    log: slog::Logger,
 }
 
 impl VmService {
-    pub fn new() -> Self {
+    pub fn new(log: slog::Logger) -> Self {
         VmService {
             handlers: Arc::new(RwLock::new(HashMap::new())),
+            log,
         }
     }
 }
@@ -27,12 +29,13 @@ impl VmService {
 #[tonic::async_trait]
 impl Node for VmService {
     async fn start_vm(&self, request: Request<VmConfig>) -> Result<Response<VmResponse>, Status> {
-        println!("Start VM: {:?}", request);
         let config = request.into_inner();
+        info!(self.log, "Starting VM {}", config.vm_id; "vm_id" => &config.vm_id, "vcpus" => &config.vcpus);
+
         let mut handlers = self.handlers.write().await;
         let handler = handlers
             .entry(config.vm_id.to_owned())
-            .or_insert(VmmHandler::new());
+            .or_insert(VmmHandler::new(self.log.new(o!())));
         handler.configure_vm(&config).await;
         handler.start_vm().await;
 
@@ -46,18 +49,29 @@ impl Node for VmService {
     }
 
     async fn stop_vm(&self, request: Request<VmId>) -> Result<Response<NodeResponse>, Status> {
-        println!("Got a request: {:?}", request);
         let vm_id = request.into_inner().vm_id;
+        info!(self.log, "Stopping VM {}", vm_id; "vm_id" => &vm_id);
+        info!(
+            self.log,
+            "{} handlers available",
+            self.handlers.read().await.len()
+        );
+
         let mut handlers = self.handlers.write().await;
-        let handler = handlers.entry(vm_id.clone()).or_insert(VmmHandler::new());
-        handler.stop_vm().await;
 
-        handlers.remove(&vm_id);
-        let response = NodeResponse {
-            status: NodeStatus::Success as i32,
-        };
+        if let Some(handler) = handlers.get(&vm_id) {
+            info!(self.log, "Fetched handler for vm {}", vm_id; "vm_id" => &vm_id);
+            handler.stop_vm().await;
 
-        Ok(Response::new(response))
+            handlers.remove(&vm_id);
+            let response = NodeResponse {
+                status: NodeStatus::Success as i32,
+            };
+
+            Ok(Response::new(response))
+        } else {
+            return Err(Status::new(Code::FailedPrecondition, "vm not found"));
+        }
     }
 
     async fn list_vms(&self, request: Request<()>) -> Result<Response<VmList>, Status> {
