2c2cdd2eb84336af4f3b2423bfa85c9844813356
Benny Zlotnik <bzlotnik@protonmail.com>
2020-06-29 20:38:53 +0300

    add keepalive

--- qarax-node/src/main.rs
+++ qarax-node/src/main.rs
@@ -5,12 +5,15 @@ use tonic::transport::Server;
 use vm_service::VmService;
 use vmm_handler::node::node_server::NodeServer;
 
+use std::time::Duration;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50051".parse()?;
     let vm_service = VmService::new();
 
     Server::builder()
+        .tcp_keepalive(Some(Duration::from_secs(60)))
         .add_service(NodeServer::new(vm_service))
         .serve(addr)
         .await?;
