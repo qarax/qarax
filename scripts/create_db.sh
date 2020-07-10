efbe328fc33164a3fa5de799f28923a6a506b63e
Daniel Douek <dadouek@gmail.com>
2020-07-10 15:23:00 +0300

    Fix scripts/create_db.sh
    Use postgres user to run the psql commands

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -2,24 +2,24 @@
 DB_NAME=${1:-"qarax"}
 DB_USER=${2:-"qarax"}
 
-existing_user=$(psql -U postgres -d template1 -c "SELECT usename FROM pg_catalog.pg_user WHERE usename = '${DB_USER}';" --tuples-only)
+existing_user=$(sudo su - postgres -c "psql -U postgres -d template1 -c \"SELECT usename FROM pg_catalog.pg_user WHERE usename = '${DB_USER}';\" --tuples-only")
 
 if [ ! -z "$existing_user" ]
 then
     echo "User already exists, not creating"
 else
     echo "Creating user ${DB_USER}..."
-    psql -U postgres -d template1 -c "create user ${DB_USER} password 'qarax';"
+    sudo su - postgres -c "psql -U postgres -d template1 -c \"create user ${DB_USER} password 'qarax';\""
 fi
 
-existing_db=$(psql -U postgres -d template1 -c "SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';" --tuples-only)
+existing_db=$(sudo su - postgres -c "psql -U postgres -d template1 -c \"SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';\" --tuples-only")
 if [ ! -z "$existing_db" ]
 then
     echo "Database already exists, not creating"
 else
-    psql -U postgres -d template1 -c "create database ${DB_NAME} owner ${DB_USER} template template0
-    encoding 'UTF8' lc_collate 'en_US.UTF-8' lc_ctype 'en_US.UTF-8';"
-    psql -U postgres -d "${DB_NAME}" -c "CREATE EXTENSION \"pgcrypto\";"
+    sudo su - postgres -c "psql -U postgres -d template1 -c \"create database ${DB_NAME} owner ${DB_USER} template template0
+    encoding 'UTF8' lc_collate 'en_US.UTF-8' lc_ctype 'en_US.UTF-8';\""
+    sudo su - postgres -c "psql -U postgres -d "${DB_NAME}" -c \"CREATE EXTENSION \"pgcrypto\";\""
 fi
 
 exit 0
