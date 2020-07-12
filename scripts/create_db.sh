ef6030ed027d93c0614023007715710ce4b0b10e
Daniel Douek <dadouek@gmail.com>
2020-07-12 12:20:31 +0300

    Fix scripts/create_db.sh (#40)
    Use postgres user to run the psql commands

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -e
 DB_NAME=${1:-"qarax"}
 DB_USER=${2:-"qarax"}
-
-existing_user=$(psql -U postgres -d template1 -c "SELECT usename FROM pg_catalog.pg_user WHERE usename = '${DB_USER}';" --tuples-only)
+SU_PREFIX="sudo su - postgres -c "
+COMMAND_PREFIX="psql -U postgres -d"
+TEMPLATE_DB="template1"
+existing_user=$(${SU_PREFIX} "${COMMAND_PREFIX} ${TEMPLATE_DB} -c \"SELECT usename FROM pg_catalog.pg_user WHERE usename = '${DB_USER}';\" --tuples-only")
 
 if [ ! -z "$existing_user" ]
 then
     echo "User already exists, not creating"
 else
     echo "Creating user ${DB_USER}..."
-    psql -U postgres -d template1 -c "create user ${DB_USER} password 'qarax';"
+    ${SU_PREFIX} "${COMMAND_PREFIX} ${TEMPLATE_DB} -c \"create user ${DB_USER} password 'qarax';\""
 fi
 
-existing_db=$(psql -U postgres -d template1 -c "SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';" --tuples-only)
+existing_db=$(${SU_PREFIX} "${COMMAND_PREFIX} ${TEMPLATE_DB} -c \"SELECT datname FROM pg_database WHERE datname = '${DB_NAME}';\" --tuples-only")
 if [ ! -z "$existing_db" ]
 then
     echo "Database already exists, not creating"
 else
-    psql -U postgres -d template1 -c "create database ${DB_NAME} owner ${DB_USER} template template0
-    encoding 'UTF8' lc_collate 'en_US.UTF-8' lc_ctype 'en_US.UTF-8';"
-    psql -U postgres -d "${DB_NAME}" -c "CREATE EXTENSION \"pgcrypto\";"
+    ${SU_PREFIX} "${COMMAND_PREFIX} ${TEMPLATE_DB} -c \"create database ${DB_NAME} owner ${DB_USER} template template0
+    encoding 'UTF8' lc_collate 'en_US.UTF-8' lc_ctype 'en_US.UTF-8';\""
+    ${SU_PREFIX} "${COMMAND_PREFIX} "${DB_NAME}" -c \"CREATE EXTENSION \"pgcrypto\";\""
 fi
 
 exit 0
