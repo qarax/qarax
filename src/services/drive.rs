20bf36e03b3453ba88bd68843f921c57a77bbf2f
Benny Zlotnik <bzlotnik@protonmail.com>
2020-08-21 16:16:51 +0300

    add Volume trait

--- src/services/drive.rs
+++ src/services/drive.rs
@@ -1,7 +1,7 @@
 use super::*;
 use crate::models::drive::{Drive, NewDrive};
+use crate::models::storage::{Storage, StorageConfig};
 use crate::models::vm::Vm;
-
 
 #[derive(Copy, Clone)]
 pub struct DriveService {}
@@ -22,4 +22,31 @@ impl DriveService {
     pub fn get_drives_for_vms(&self, vm: &Vm, conn: &DbConnection) -> Result<Vec<Drive>> {
         Drive::get_drives_for_vm(vm, conn)
     }
+
+    pub fn get_storage(&self, drive_id: String, conn: &DbConnection) -> Result<Storage> {
+        Drive::get_storage(Uuid::parse_str(&drive_id)?, conn)
+    }
 }
+
+pub trait Volume {
+    fn get_path(&self) -> String;
+}
+
+pub struct LocalVolume<'a> {
+    drive_id: &'a str,
+    storage: &'a StorageConfig,
+}
+
+impl<'a> LocalVolume<'a> {
+    pub fn new(drive_id: &'a str, storage: &'a StorageConfig) -> LocalVolume<'a> {
+        LocalVolume { drive_id, storage }
+    }
+}
+
+impl<'a> Volume for LocalVolume<'a> {
+    fn get_path(&self) -> String {
+        format!("{}/{}", self.storage.path.as_ref().unwrap(), self.drive_id)
+    }
+}
+
+pub struct SharedVolume {}
