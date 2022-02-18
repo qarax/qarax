c3312c8eb68016a524e98e4b22806296a07e7cb7
Benny Zlotnik <bzlotnik@protonmail.com>
2022-02-18 18:45:45 +0200

    format e2e tests with black

--- e2e/e2e.py
+++ e2e/e2e.py
@@ -15,8 +15,9 @@ log = logging.getLogger(__name__)
 
 @pytest.fixture(scope="module", autouse=True)
 def tf():
-    tf = terraform.Terraform(workdir=os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'terraform'))
+    tf = terraform.Terraform(
+        workdir=os.path.join(os.path.abspath(os.path.dirname(__file__)), "terraform")
+    )
 
     yield tf
 
@@ -27,7 +28,7 @@ def vm(tf):
     if err:
         log.error(err)
         raise Exception("Failed to init terraform plan")
-        
+
     _, err = tf.apply()
     if err:
         log.error(err)
@@ -46,9 +47,9 @@ def vm_ip(vm):
     log.info("Getting VM IP address")
     vm_ip = None
 
-    for resource in vm['values']['root_module']['resources']:
-        if 'network_interface' in resource['values']:
-            vm_ip = resource['values']['network_interface'][0]['addresses'][0]
+    for resource in vm["values"]["root_module"]["resources"]:
+        if "network_interface" in resource["values"]:
+            vm_ip = resource["values"]["network_interface"][0]["addresses"][0]
 
     if vm_ip is None:
         raise Exception("VM IP address not found")
@@ -78,56 +79,71 @@ def vm_drives(vm_ip, host_config):
     rootfs = "rootfs/bionic.rootfs.ext4"
     base_url = "https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/x86_64/"
 
-    util.run_parallel_jobs([functools.partial(util.download_file_to_host, url=base_url + kernel, host=vm_ip,
-                                              username=host_config['username'], password=host_config['password'], dest_path=f'/root/{kernel.split("/")[1]}'),
-                            functools.partial(util.download_file_to_host, url=base_url + rootfs, host=vm_ip,
-                                              username=host_config['username'], password=host_config['password'], dest_path=f'/root/{rootfs.split("/")[1]}')])
-
-
-@ pytest.fixture(scope="module", autouse=True)
-def qarax_configuration():
-    configuration = qarax.Configuration(
-        host="http://localhost:3000"
+    util.run_parallel_jobs(
+        [
+            functools.partial(
+                util.download_file_to_host,
+                url=base_url + kernel,
+                host=vm_ip,
+                username=host_config["username"],
+                password=host_config["password"],
+                dest_path=f'/root/{kernel.split("/")[1]}',
+            ),
+            functools.partial(
+                util.download_file_to_host,
+                url=base_url + rootfs,
+                host=vm_ip,
+                username=host_config["username"],
+                password=host_config["password"],
+                dest_path=f'/root/{rootfs.split("/")[1]}',
+            ),
+        ]
     )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def qarax_configuration():
+    configuration = qarax.Configuration(host="http://localhost:3000")
 
     yield configuration
 
 
-@ pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def api_client(qarax_configuration):
     api_client = qarax.ApiClient(qarax_configuration)
 
     yield api_client
 
 
-@ pytest.mark.order(1)
+@pytest.mark.order(1)
 def test_install_host(api_client, vm_ip, host_config):
     api_instance = hosts_api.HostsApi(api_client)
     host = Host(
-        name=host_config['name'],
-        address=host_config['address'],
-        host_user=host_config['username'],
-        password=host_config['password'],
-        port=host_config['port'],
+        name=host_config["name"],
+        address=host_config["address"],
+        host_user=host_config["username"],
+        password=host_config["password"],
+        port=host_config["port"],
     )
 
     def get_host_status():
-        host = api_instance.get_host(host_id)['response']
-        return host['status']
+        host = api_instance.get_host(host_id)["response"]
+        return host["status"]
 
     try:
         log.info("Adding host to database")
         api_response = api_instance.add_host(host)
-        host_id = api_response['response']
-        if host_id != 'error':
+        host_id = api_response["response"]
+        if host_id != "error":
             log.info("Installing host '%s'", host_id)
             api_instance.install_host(host_id)
             assert util.wait_for_status(
-                get_host_status, status='up', timeout=120, step=5)
+                get_host_status, status="up", timeout=120, step=5
+            )
 
     except qarax.ApiException as e:
         log.error("Exception when calling HostsApi->add_host: %s\n" % e)
         raise e
 
     healthcheck = api_instance.healthcheck(host_id)
-    assert healthcheck['response'] == 'ok'
+    assert healthcheck["response"] == "ok"
